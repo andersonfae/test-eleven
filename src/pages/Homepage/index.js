a08a6581a0f2fb07c878e8a6b49d93b03b99ea5c
Anderson Fae <andersonfae.owl@gmail.com>
2022-08-24 12:41:42 -0300

    Search bar and Status filter done

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -4,12 +4,27 @@ import { useEffect, useState } from "react";
 import { Navbar } from "../../components/Navbar";
 
 export function Homepage() {
-  // const [user, setUsers] = useState([]);
+  const [newuser, setNewUsers] = useState({
+    name: "",
+    email: "",
+    status: "",
+  });
+
+  // const regexEmail = RegExp(^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$)
 
   return (
     <>
       <Navbar />
-      <div></div>
+      <form>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input id="name" type="string" />
+          <label htmlFor="email">Email:</label>
+          <input id="email" type="string" />
+          <label htmlFor="status">Status:</label>
+          <input id="status" type="boolean" />
+        </div>
+      </form>
     </>
   );
 }
--- src/pages/Users/index.js
+++ src/pages/Users/index.js
@@ -3,10 +3,13 @@ import { Cards } from "../../components/Cards";
 import axios, { Axios } from "axios";
 
 export function Users() {
-  const [user, setUsers] = useState([]);
-  console.log(user);
+  const [users, setUsers] = useState([]);
+
+  const [filteredUsers, setFilteredUsers] = useState(users);
 
   const [search, setSearch] = useState("");
+
+  const [checkStatus, setCheckStatus] = useState("all");
 
   useEffect(() => {
     async function fetchUsers() {
@@ -22,32 +25,71 @@ export function Users() {
     fetchUsers();
   }, []);
 
+  useEffect(() => {
+    setFilteredUsers(users);
+  }, [users, setUsers]);
+
+  useEffect(() => {
+    const usersByName = users.filter((user) =>
+      user.name.toLowerCase().includes(search.toLowerCase())
+    );
+    const usersByActive = usersByName.filter((user) => {
+      if (checkStatus === "all") return true;
+      return user.status === checkStatus;
+    });
+    setFilteredUsers(usersByActive);
+  }, [setSearch, search, checkStatus, setCheckStatus]);
+
   function handleSearch(e) {
     setSearch(e.target.value);
+  }
+
+  function handleActive(e) {
+    setCheckStatus(e.target.value);
+    console.log(e.target.value);
   }
 
   return (
     <>
       <div classname="">
         <input name="search" placeholder="Find User" onChange={handleSearch} />
-        {user
-          .filter((currentElement) => {
-            return currentElement.name
-              .toLowerCase()
-              .includes(search.toLowerCase());
-          })
-          .map((currentElement, key) => {
-            return (
-              <div key={key}>
-                <Cards
-                  name={currentElement.name}
-                  email={currentElement.email}
-                  status={currentElement.status}
-                  id={currentElement.id}
-                />
-              </div>
-            );
-          })}
+        <input
+          type="radio"
+          checked={checkStatus === "active"}
+          value="active"
+          id="selectActive"
+          onChange={handleActive}
+        />
+        <label for="selectActive">Active</label>
+        <input
+          type="radio"
+          value="inactive"
+          checked={checkStatus === "inactive"}
+          id="selectActive"
+          onChange={handleActive}
+        />
+        <label for="selectActive">Inactive</label>
+        <input
+          type="radio"
+          value="all"
+          checked={checkStatus === "all"}
+          id="selectActive"
+          onChange={handleActive}
+        />
+        <label for="selectActive">All Users</label>
+
+        {filteredUsers.map((currentElement, key) => {
+          return (
+            <div key={key}>
+              <Cards
+                name={currentElement.name}
+                email={currentElement.email}
+                status={currentElement.status}
+                id={currentElement.id}
+              />
+            </div>
+          );
+        })}
       </div>
     </>
   );
